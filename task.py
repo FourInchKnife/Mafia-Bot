c71336a2df3bdc973b68d5c5d4f387092f39d18c
rxal12233445 <35819417+rxal12233445@users.noreply.github.com>
2020-06-20 11:45:13 -0400

    total rewrite

--- task.py
+++ task.py
@@ -1,40 +1,35 @@
 from os import environ
-import discord
-bot = discord.Client()
+from discord.ext import commands
+bot = commands.Bot(command_prefix='!',owner_id=600130839870963725)
 @bot.event
 async def on_ready():
-    print('We have logged in as {}'.format(bot.user))
-    await bot.change_presence(activity=discord.Game("!vote and !kill"))
-@bot.event
-async def on_message(message):
-    if message.content.startswith('!'):
-        cmd=message.content[1:]
-        params=cmd.split(" ",1)[:]
-        if params[0] in ['vote','kill']:
-            if params[0]=='vote':
-                guildVillagers=[]
-                for Person in message.channel.guild.members:
-                    for Role in Person.roles:
-                        if Role.name == 'Alive':
-                            guildVillagers.append(Person)
-                toSend='Who do you want to put on trial?'
-                for i in guildVillagers:
-                    x='0123456789abcdefghij'[guildVillagers.index(i)]
-                    toSend+='\n'+x+') `'+i.display_name+'`'
-                aliveMention='"Alive"'
-                if len(guildVillagers)==0:
-                    for i in message.channel.guild.roles:
-                        if i.name=="Alive":
-                            aliveMention=i.mention
-                    toSend='There is no one to put on trial! Try giving some people the {} role to get started.'.format(aliveMention)
-                sentMessage=await message.channel.send(toSend)
-                for i in range(len(guildVillagers)):
-                    await sentMessage.add_reaction(('0️⃣','1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣','🇦','🇧','🇨','🇩','🇪','🇫','🇬','🇭','🇮','🇯')[i])
-            elif params[0]=='kill':
-                sentMessage=await message.channel.send('Do you want to kill '+params[1]+'?')
-                await sentMessage.add_reaction('\U00002705')
-                await sentMessage.add_reaction('\U0000274C')
-
+    print('Logged in as {}'.format(bot.user))
+    #presence here later
+@bot.command
+async def vote(ctx,param):
+    guildVillagers=[]
+        for Person in ctx.guild.members:
+            for Role in Person.roles:
+                if Role.name == 'Alive':
+                        guildVillagers.append(Person)
+        toSend='Who do you want to put on trial?'
+        for i in guildVillagers:
+            x='0123456789abcdefghij'[guildVillagers.index(i)]
+            toSend+='\n'+x+') `'+i.display_name+'`'
+        aliveMention='"Alive"'
+        if len(guildVillagers)==0:
+            for i in ctx.guild.roles:
+                if i.name=="Alive":
+                    aliveMention=i.mention
+            toSend='There is no one to put on trial! Try giving some people the {} role to get started.'.format(aliveMention)
+        sentMessage=await ctx.send(toSend)
+        for i in range(len(guildVillagers)):
+            await sentMessage.add_reaction(('0️⃣','1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣','🇦','🇧','🇨','🇩','🇪','🇫','🇬','🇭','🇮','🇯')[i])
+@bot.command
+async def kill(ctx,param):
+    sentMessage=await ctx.send('Do you want to kill '+param+'?')
+    await sentMessage.add_reaction('\U00002705')
+    await sentMessage.add_reaction('\U0000274C')
 bot_token=environ.get('BOT_TOKEN',None)
 if not bot_token:
     bot_token=input('What is your bot token?')
