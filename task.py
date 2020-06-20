809b9726b717221625fd5bf437d5a0b9cc433371
rxal12233445 <35819417+rxal12233445@users.noreply.github.com>
2020-06-20 12:18:30 -0400

    Update task.py

--- task.py
+++ task.py
@@ -27,7 +27,7 @@ async def vote(ctx):
         await sentMessage.add_reaction(('0️⃣','1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣','🇦','🇧','🇨','🇩','🇪','🇫','🇬','🇭','🇮','🇯')[i])
 @bot.command()
 async def kill(ctx,arg):
-    sentMessage=await ctx.send('Do you want to kill '+arg+'?')
+    sentMessage=await ctx.send('Do you want to kill `'+arg+'`?')
     await sentMessage.add_reaction('\U00002705')
     await sentMessage.add_reaction('\U0000274C')
 bot_token=environ.get('BOT_TOKEN',None)
